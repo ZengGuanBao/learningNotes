7cf1fccf78f9d8a0ef91cec5c379a76295c34d71
ZengGuanBao <672280840@qq.com>
2019-05-22 17:28:48 +0800

    vue-il8n的版本1.0.0.0 init

---
+++ vue-il8n/src/assets/i18n/en.js
@@ -0,0 +1,9 @@
+export const m = {
+  music: 'Music',
+  findMusic: 'FIND MUSIC',
+  myMusic: 'MY MUSIC',
+  friend: 'FRIEND',
+  musician: 'MUSICIAN',
+  nav: 'Navigation Column',
+  msg: 'Welcome to Your Vue.js App'
+}
---
+++ vue-il8n/src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <img @click="changeLangEvent" src="../assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2 v-text="$t('m.nav')"></h2>
+    <ul>
+      <li>{{$t('m.music')}}</li>
+      <li>{{$t('m.findMusic')}}</li>
+      <li>{{$t('m.myMusic')}}</li>
+      <li>{{$t('m.friend')}}</li>
+      <li>{{$t('m.musician')}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: this.$t('m.msg')
+    }
+  },
+  methods: {
+    changeLangEvent () {
+      // this.$confirm('确定切换语言吗?', '提示', {
+      //   confirmButtonText: '确定',
+      //   cancelButtonText: '取消',
+      //   type: 'warning'
+      // }).then(() => {
+      if (this.lang === 'zh-CN') {
+        this.lang = 'en-US'
+        this.$i18n.locale = this.lang
+      } else {
+        this.lang = 'zh-CN'
+        this.$i18n.locale = this.lang
+      }
+      // }).catch(() => {
+      //   this.$message({
+      //     type: 'info'
+      //   })
+      // })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
